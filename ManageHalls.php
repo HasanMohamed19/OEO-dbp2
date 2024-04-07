828994d9a4c9e64dfa8db3540b15f7da1c5aa874
FatimaMahfoodh <139788668+FatimaMahfoodh@users.noreply.github.com>
2024-04-07 17:36:00 +0300

    Created Manage Halls Form - needs few style modifications

--- ManageHalls.php
+++ ManageHalls.php
@@ -5,27 +5,5 @@
  */
 include './template/header.html';
 include './styles.css';
-?>
-<div class="row">
-    <div class="col-xl-12">
-          <button type="button" class="btn btn-primary">+ New Hall</button>
-    </div>
-</div>
-<div class="row">
-    <div class="col-xl-4 bg-primary text-white">
-        50%
-
-    </div>
-
-    <div class="col-xl-8 bg-dark text-white">
-        50%
-    </div>
-</div>
-<br>
-
-
-<?php
-echo '<div class="main"> </div>';
-
+include './template/ManageHalls.html';
 include './template/footer.html';
-?>
