09f3e5ead21ae93b0db1b8d59c5fd30dbdb4f98c
FatimaMahfoodh <139788668+FatimaMahfoodh@users.noreply.github.com>
2024-04-07 13:08:54 +0300

    Created ManageHalls.php file

---
+++ ManageHalls.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+include './template/header.html';
+include './styles.css';
+?>
+<div class="row">
+    <div class="col-xl-12">
+          <button type="button" class="btn btn-primary">+ New Hall</button>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xl-4 bg-primary text-white">
+        50%
+
+    </div>
+
+    <div class="col-xl-8 bg-dark text-white">
+        50%
+    </div>
+</div>
+<br>
+
+
+<?php
+echo '<div class="main"> </div>';
+
+include './template/footer.html';
+?>
