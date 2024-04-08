5cb0a8596529e1094c597d5f97377ed02d7f8be7
FatimaMahfoodh <139788668+FatimaMahfoodh@users.noreply.github.com>
2024-04-08 16:44:22 +0300

    Created Admin Browse Halls Card

---
+++ Admin_ViewHalls.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+include './template/header.html';
+include './template/Admin_ViewHalls.html';
+include './template/footer.html';
