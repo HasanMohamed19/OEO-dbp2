96acc924a862f0a7c3f70915750e00beb0bd920b
theMycron <134844972+theMycron@users.noreply.github.com>
2024-05-12 21:12:33 +0300

    moved form pagination code into functions added invalid input feedback added some validations

--- javascript/client_booking.js
+++ javascript/client_booking.js
@@ -2,6 +2,7 @@ const cateringItemDisplayCount = 10;
 const hallId = (new URL(location.href)).searchParams.get('hallId');
 const clientId = 1;
 var hallRentalCharge; // will be set after side menu loads
+var hallCapacity;
 
 $('#bookingForm').on('submit', function(event) {
     // post request must be made with js to allow sending
@@ -39,6 +40,8 @@ $('#bookingForm').on('submit', function(event) {
 });
 
 $(document).ready(function() {
+    enableCateringMenuTabs();
+    addPageButtonListeners();
     updateSideMenu();
     // load pages for first menu
     enablePagination(1, ".cateringItem", cateringItemDisplayCount);
@@ -55,20 +58,22 @@ const getServiceIdFromName = (serviceName) => {
     else if (serviceName === "cold") {return 4;}
 };
 
-// enable catering menu tabs
-const triggerTabList = document.querySelectorAll('#cateringTabBar button');
-triggerTabList.forEach(triggerEl => {
-  const tabTrigger = new bootstrap.Tab(triggerEl);
+const enableCateringMenuTabs = () => {
+    // enable catering menu tabs
+    const triggerTabList = document.querySelectorAll('#cateringTabBar button');
+    triggerTabList.forEach(triggerEl => {
+      const tabTrigger = new bootstrap.Tab(triggerEl);
 
-  triggerEl.addEventListener('click', event => {
-//      console.log("CLICKED ON TABBB");
-    event.preventDefault();
-    const menuType = triggerEl.id.split("-",1)[0];
-//    updateCateringMenu(getServiceIdFromName(menuType),1,10);
-    enablePagination(getServiceIdFromName(menuType), ".cateringItem", cateringItemDisplayCount);
-    tabTrigger.show();
-  });
-});
+      triggerEl.addEventListener('click', event => {
+    //      console.log("CLICKED ON TABBB");
+        event.preventDefault();
+        const menuType = triggerEl.id.split("-",1)[0];
+    //    updateCateringMenu(getServiceIdFromName(menuType),1,10);
+        enablePagination(getServiceIdFromName(menuType), ".cateringItem", cateringItemDisplayCount);
+        tabTrigger.show();
+      });
+    });
+};
 
 //      Catering Item Quantity and Checkbox 
 // disable quantity box based on checkbox for all menu items
@@ -107,97 +112,129 @@ const clearCardForm = () => {
 };
 
 
-//      Previous/Next buttons functionality
-let currentSection = 0;
-let sections = document.querySelectorAll("#bookingForm > fieldset");
-let sectionButtons = document.querySelectorAll("#bookingProgress > li");
-let cards = document.querySelectorAll(".clientBookingSidebar > .clientCard");
-let nextButton = document.querySelector(".next");
-let saveButton = document.querySelector(".save");
-let previousButton = document.querySelector(".previous");
 
-const checkValidity = (element) => {
-    return false;
+const checkEventValidity = () => {
+    
+    
+    let audienceNumber = parseInt($("#bookingNoAudiences").val());
+    if (audienceNumber === null 
+            || audienceNumber > hallCapacity 
+            || audienceNumber <= 0) {
+        $("#bookingNoAudiences").addClass('is-invalid');
+        return false;
+    }
+    
+    let startDate = $("#bookingStartDate").val();
+    if (startDate === null) {
+        $("#bookingStartDate").addClass('is-invalid');
+        return false;
+    }
+    if (new Date(startDate).getDate() <= new Date().getDate()) {
+        $("#bookingStartDate").addClass('is-invalid');
+        return false;
+    }
+    // validate if hall is already booked at this time
+    
+    return true;
 };
 
-for (let i = 0; i < sectionButtons.length; i++) {
-//    console.log("Added listener to " + sectionButtons[i]);
-    sectionButtons[i].addEventListener("click", function() {
-        $("#eventFieldSet").addClass('was-validated');
-        console.log(checkValidity($("#eventFieldSet")));
-        if (!checkValidity($("#eventFieldSet"))) {
-            return;
-        }
-        updateSideMenu();
-//        console.log("Clicked on me!!");
-        sections[currentSection].classList.remove("active");
-        sectionButtons[currentSection].classList.remove("active");
-        // if going to previous section, make current card inactive
-        if (currentSection > i && currentSection < cards.length) {
-            for (j = currentSection; j>i; j--) {
-                cards[j].classList.add("inactive");
+//      Previous/Next buttons functionality
+const addPageButtonListeners = () => {
+    let currentSection = 0;
+//    let sections = document.querySelectorAll("#bookingForm > fieldset");
+    let sectionButtons = document.querySelectorAll("#bookingProgress > li");
+    let nextButton = document.querySelector(".next");
+    let saveButton = document.querySelector(".save");
+    let previousButton = document.querySelector(".previous");
+    for (let i = 0; i < sectionButtons.length; i++) {
+    //    console.log("Added listener to " + sectionButtons[i]);
+        sectionButtons[i].addEventListener("click", function() {
+            console.log('trying to change page. clicked on num: '+i+'. coming from: '+currentSection);
+            if (i === 0) {
+                if (previousButton.className.split(" ").indexOf("hide") < 0) {
+                    previousButton.classList.add("hide");
+                }
+            } else {
+                // validate event if leaving first page
+                $("#eventFieldSet").addClass('was-validated');
+                console.log(checkEventValidity());
+                if (!checkEventValidity()) {
+                    return;
+                }
+
+                if (previousButton.className.split(" ").indexOf("hide") >= 0) {
+                    previousButton.classList.remove("hide");
+                }
             }
-        }
-        
-        sections[currentSection = i].classList.add("active");
-        sectionButtons[currentSection].classList.add("active");
-        if (currentSection < cards.length) {
-            for (j = 0; j<=currentSection; j++) {
-                cards[j].classList.remove("inactive");
+            if (i === sectionButtons.length - 1) {
+                console.log("setting total cost now");
+                setTotalCost(); // display cost at the bottom if this is last page
+
+                // for last page, show save button instead of next
+                if (nextButton.className.split(" ").indexOf("hide") < 0) {
+                    nextButton.classList.add("hide");
+                }
+                if (saveButton.className.split(" ").indexOf("hide") >= 0) {
+                    saveButton.classList.remove("hide");
+                }
+            } else {
+                if (nextButton.className.split(" ").indexOf("hide") >= 0) {
+                    nextButton.classList.remove("hide");
+                }
+                if (saveButton.className.split(" ").indexOf("hide") < 0) {
+                    saveButton.classList.add("hide");
+                }
             }
-        }
-        
-        if (i === 0) {
-            if (previousButton.className.split(" ").indexOf("hide") < 0) {
-                previousButton.classList.add("hide");
-            }
-        } else {
-            if (previousButton.className.split(" ").indexOf("hide") >= 0) {
-                previousButton.classList.remove("hide");
-            }
-        }
-        if (i === sectionButtons.length - 1) {
-            console.log("setting total cost now");
-            setTotalCost(); // display cost at the bottom if this is last page
-            
-            // for last page, show save button instead of next
-            if (nextButton.className.split(" ").indexOf("hide") < 0) {
-                nextButton.classList.add("hide");
-            }
-            if (saveButton.className.split(" ").indexOf("hide") >= 0) {
-                saveButton.classList.remove("hide");
-            }
-        } else {
-            if (nextButton.className.split(" ").indexOf("hide") >= 0) {
-                nextButton.classList.remove("hide");
-            }
-            if (saveButton.className.split(" ").indexOf("hide") < 0) {
-                saveButton.classList.add("hide");
-            }
+            currentSection = changePage(currentSection, i);
+        });
+    }
+    nextButton.addEventListener("click", function() {
+            console.log("Tried to click");
+        if (currentSection < sectionButtons.length - 1) {
+            sectionButtons[currentSection + 1].click();
+            console.log("clicked");
         }
     });
-}
 
+    previousButton.addEventListener("click", function() {
+        if (currentSection > 0) {
+            sectionButtons[currentSection - 1].click();
+        }
+    });
 
-nextButton.addEventListener("click", function() {
-//        console.log("Tried to click");
-    if (currentSection < sectionButtons.length - 1) {
-        sectionButtons[currentSection + 1].click();
+    //  add function for complete booking button
+    saveButton.addEventListener("click", function() {
+        // take user to summary page
+    //    window.location.href = "booking_summary.php";
+    });
+};
+const changePage = (currentSection, nextSection) => {
+    let sections = document.querySelectorAll("#bookingForm > fieldset");
+    let sectionButtons = document.querySelectorAll("#bookingProgress > li");
+    let cards = document.querySelectorAll(".clientBookingSidebar > .clientCard");
+    updateSideMenu();
+    // show selected page
+    sections[currentSection].classList.remove("active");
+    sectionButtons[currentSection].classList.remove("active");
+    // if going to previous section, make current card inactive
+    if (currentSection > nextSection && currentSection < cards.length) {
+        for (j = currentSection; j>nextSection; j--) {
+            cards[j].classList.add("inactive");
+        }
     }
-});
 
-previousButton.addEventListener("click", function() {
-    if (currentSection > 0) {
-        sectionButtons[currentSection - 1].click();
+    sections[currentSection = nextSection].classList.add("active");
+    sectionButtons[currentSection].classList.add("active");
+    if (currentSection < cards.length) {
+        for (j = 0; j<=currentSection; j++) {
+            cards[j].classList.remove("inactive");
+        }
     }
-});
+    return currentSection;
+};
 
 
-//  add function for complete booking button
-saveButton.addEventListener("click", function() {
-    // take user to summary page
-//    window.location.href = "booking_summary.php";
-});
+
 
 
 //   Update values for side menu
@@ -234,6 +271,7 @@ const updateSideMenu = () => {
         let data = JSON.parse(res);
 //        if (data.error)
         hallRentalCharge = data.rentalCharge;
+        hallCapacity = data.capacity;
         updateHallDetails(
                 data.hallName,
                 data.hallDescription,
