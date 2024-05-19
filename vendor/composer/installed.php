6d7d4a22f42570c4c236773cba8b7531fbf362ec
Hasan <hzs2003@live.com>
2024-05-19 09:40:57 +0300

    aded phpMailer package

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/o-e-o-system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ffe61e5f686ee98adf53bc5176f1ac69dffaacc6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.1',
+            'version' => '6.9.1.0',
+            'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vendor/o-e-o-system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ffe61e5f686ee98adf53bc5176f1ac69dffaacc6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
