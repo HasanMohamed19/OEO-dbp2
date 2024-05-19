d8a471c6aa1dee113d81e2c8bb20c393c4279127
FatimaMahfoodh <139788668+FatimaMahfoodh@users.noreply.github.com>
2024-05-19 22:51:45 +0300

    worked on home page

--- models/Hall.php
+++ models/Hall.php
@@ -10,6 +10,14 @@
  *
  * @author Hassan
  */
+include_once '../helpers/Database.php';
+include_once 'models/HallImage.php';
+
+//hall status
+
+const AVAILABLE_STATUS = 1;
+const CANCELLED_STATUS = 2;
+
 class Hall {
 
     private $hallId;
@@ -17,7 +25,10 @@ class Hall {
     private $description;
     private $rentalCharge;
     private $capacity;
-    private $imagePath;
+//    private $imagePath;
+//    private $images;
+    private $hallStatus;
+    private $deletedImages;
 
     public function __construct() {
         $this->hallId = null;
@@ -25,22 +36,30 @@ class Hall {
         $this->description = null;
         $this->rentalCharge = null;
         $this->capacity = null;
-        $this->imagePath = null;
+        $this->hallStatus = null;
+//        $this->imagePath = null;
+//        $this->images = [];
+        $this->deletedImages = [];
     }
 
-    public function initWith($hallId, $hallName, $description, $rentalCharge, $capacity, $imagePath) {
+    public function initWith($hallId, $hallName, $description, $rentalCharge, $capacity, $hallStatus) {
         $this->hallId = $hallId;
         $this->hallName = $hallName;
         $this->description = $description;
         $this->rentalCharge = $rentalCharge;
         $this->capacity = $capacity;
-        $this->imagePath = $imagePath;
+        $this->hallStatus = $hallStatus;
+//        $this->imagePath = $imagePath;
     }
 
-    public function initWithHallId($hallId) {
+    public function initWithHallid($id) {
         $db = Database::getInstance();
-        $data = $db->singleFetch('SELECT * FROM dbProj_Hall WHERE hall_id = ' . $hallId);
-        $this->initWith($data->hall_id, $data->hall_name, $data->description, $data->rental_charge, $data->capacity, $data->image_path);
+        $data = $db->singleFetch('SELECT * FROM dbProj_Hall WHERE hall_id = ' . $id);
+        $this->initWith($data->hall_id, $data->hall_name, $data->description, $data->rental_charge, $data->capacity, $data->hall_status_id);
+//        $hallImages = $db->multiFetch('Select * from dbProj_Hall_Image where hall_id ='.$id);
+//        for ($i=0;$i<count($hallImages);$i++){
+//            $this->images = $hallImages[$i]->hall_image_path;
+//        }
     }
 
     public function getHallId() {
@@ -63,8 +82,11 @@ class Hall {
         return $this->capacity;
     }
 
-    public function getImagePath() {
-        return $this->imagePath;
+//    public function getImagePath() {
+//        return $this->imagePath;
+//    }
+    public function getHallStatus() {
+        return $this->hallStatus;
     }
 
     public function setHallId($hallId) {
@@ -87,17 +109,225 @@ class Hall {
         $this->capacity = $capacity;
     }
 
-    public function setImagePath($imagePath) {
-        $this->imagePath = $imagePath;
+    public function setHallStatus($hallStatus) {
+        echo 'set hall status to ' . $hallStatus;
+        $this->hallStatus = $hallStatus;
+    }
+
+//    public function setImagePath($imagePath) {
+//        $this->imagePath = $imagePath;
+//    }
+
+    public function addHallImages($hallImages) {
+        for ($i = 0; $i < count($hallImages); $i++) {
+            $hallImg = new HallImage();
+            $hallImg->setHall_id($this->hallId);
+            echo'<h1>Hall id is</h1>' . $this->hallId;
+            $hallImg->setHallImagePath($hallImages[$i]);
+            $hallImg->addHallImage();
+        }
+    }
+
+    function getAllHalls() {
+        $db = Database::getInstance();
+        // get only active halls
+        $data = $db->multiFetch('Select * from dbProj_Hall WHERE hall_status_id != 2');
+        return $data;
+    }
+
+    function getHallsBySearch($search) {
+        $db = new Database();
+        $searhTerm = $db->sanitizeString($search);
+        $q = "SELECT * FROM dbProj_Hall WHERE hall_status_id != 2 AND MATCH(hall_name, description) against (?)";
+
+        $stmt = mysqli_prepare($db->getDatabase(), $q);
+
+        if (!$stmt) {
+            $db->displayError($q);
+            return false;
+        }
+        $stmt->bind_param('s', $searhTerm);
+        if (!$stmt->execute()) {
+//                var_dump($stmt);
+            echo 'Execute failed';
+            $db->displayError($q);
+            return false;
+        }
+        $result = $stmt->get_result();
+//        var_dump($result);
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+//        var_dump($data);
+        return $data;
+    }
+
+    function addHall() {
+        $db = new Database();
+        if ($this->isValid()) {
+            $this->hallName = $db->sanitizeString($this->hallName);
+            $this->description = $db->sanitizeString($this->description);
+            $this->rentalCharge = $db->sanitizeString($this->rentalCharge);
+            $this->capacity = $db->sanitizeString($this->capacity);
+//            $this->imagePath = $db->sanitizeString($this->imagePath);
+            $this->hallStatus = $db->sanitizeString($this->hallStatus);
+
+            $q = "INSERT INTO dbProj_Hall(hall_name,description,rental_charge,capacity,hall_status_id) VALUES(?,?,?,?,?)";
+
+            $stmt = mysqli_prepare($db->getDatabase(), $q);
+
+            if ($stmt) {
+                $stmt->bind_param('ssdii', $this->hallName, $this->description, $this->rentalCharge, $this->capacity, $this->hallStatus);
+                if (!$stmt->execute()) {
+                    var_dump($stmt);
+                    echo 'Execute failed';
+                    $db->displayError($q);
+                    return false;
+                }
+            } else {
+                $db->displayError($q);
+                return false;
+            }
+            $this->hallId = $db->singleFetch("SELECT hall_id FROM dbProj_Hall WHERE hall_name = '" . $this->hallName . '\'')->hall_id;
+            return true;
+        } else {
+            echo'invalid inputs';
+            return false;
+        }
+    }
+
+    function deleteHall() {
+        $db = new Database();
+        $this->hallId = $db->sanitizeString($this->hallId);
+        $q = "Delete from dbProj_Hall where hall_id=?";
+        $stmt = mysqli_prepare($db->getDatabase(), $q);
+
+        if ($stmt) {
+            $stmt->bind_param('i', $this->hallId);
+            if (!$stmt->execute()) {
+                var_dump($stmt);
+                echo 'Execute failed';
+                $db->displayError($q);
+                return false;
+            }
+        } else {
+            $db->displayError($q);
+            return false;
+        }
+        return true;
+    }
+
+    function updateHall() {
+        $db = new Database();
+        if ($this->isValid()) {
+            $this->hallId = $db->sanitizeString($this->hallId);
+            $this->hallName = $db->sanitizeString($this->hallName);
+            $this->description = $db->sanitizeString($this->description);
+            $this->rentalCharge = $db->sanitizeString($this->rentalCharge);
+            $this->capacity = $db->sanitizeString($this->capacity);
+            $this->hallStatus = $db->sanitizeString($this->hallStatus);
+            $q = "UPDATE dbProj_Hall set hall_name = ? ,description = ?  ,rental_charge = ? ,capacity = ?, hall_status_id = ? WHERE hall_id = ? ";
+
+            $stmt = mysqli_prepare($db->getDatabase(), $q);
+
+            if ($stmt) {
+                $stmt->bind_param('ssdiii', $this->hallName, $this->description, $this->rentalCharge, $this->capacity, $this->hallStatus, $this->hallId);
+                if (!$stmt->execute()) {
+                    var_dump($stmt);
+                    echo 'Execute failed';
+                    $db->displayError($q);
+                    return false;
+                }
+            } else {
+                $db->displayError($q);
+                return false;
+            }
+            return true;
+        } else {
+            echo'invalid values :(';
+        }
+    }
+
+    function getHallStatusName() {
+        $db = Database::getInstance();
+        $data = $db->singleFetch("SELECT status_name FROM dbProj_Availability_Status a JOIN dbProj_Hall h ON h.hall_status_id = a.availability_status_id WHERE h.hall_id = '$this->hallId'");
+//       var_dump($data);
+        return $data;
+    }
+
+    public function isValid() {
+        $errors = array();
+
+        if (empty($this->hallName))
+            $errors[] = 'You must enter a Hall Name';
+
+        if (empty($this->rentalCharge))
+            $errors[] = 'You must enter a Rental Charge';
+
+        if (empty($this->capacity))
+            $errors[] = 'You must enter a Capacity';
+        if (empty($this->hallStatus))
+            $errors[] = 'You must enter a status';
+        if (empty($errors))
+            return true;
+        else
+            return false;
+    }
+
+    public static function getAvailableHalls($startDate, $endDate) {
+        include_once './models/Event.php';
+        // returns halls that are available at the selected timeframe
+        $availableHalls = [];
+        $h = new Hall();
+        $halls = $h->getAllHalls();
+        foreach ($halls as $hall) {
+            $events = Event::getEventsForHall($hall->hall_id);
+            $isOverlapping = false;
+            foreach ($events as $event) {
+                $event = (object) $event;
+                $eStartDate = $event->start_date;
+                $eEndDate = $event->end_date;
+
+//                echo "For hall $hall->hall_name, checking event $event->event_name."
+//                        . " Currently, start date for event is $eStartDate"
+//                        . " and end date is $eEndDate.";
+                // check if event timeframe overlaps requested timeframe
+                if (self::areDatesOverlapping($startDate, $endDate, $eStartDate, $eEndDate)) {
+                    // there is overlap, meaning hall is already booked at this time
+                    $isOverlapping = true;
+                    break;
+                }
+            }
+            // if no timeframes overlap, this hall is available
+            if (!$isOverlapping) {
+                $availableHalls[] = $hall;
+            }
+        }
+//        echo 'Available halls found are: ';
+//        var_dump($availableHalls);
+        return $availableHalls;
+    }
+
+    public static function areDatesOverlapping($startDate1, $endDate1, $startDate2, $endDate2) {
+        return ($startDate1 >= $startDate2 && $startDate1 <= $endDate2) || ($endDate1 >= $startDate2 && $endDate1 <= $endDate2) || ($startDate2 >= $startDate1 && $startDate2 <= $endDate1);
+    }
+
+    public static function getMaxCapacity() {
+        $db = Database::getInstance();
+        // get only active halls
+        $data = $db->singleFetch('Select MAX(capacity) as max_capacity from dbProj_Hall WHERE hall_status_id != 2');
+        return $data->max_capacity;
     }
 
     function getPopularHalls() {
         $db = new Database();
-        $q = "SELECT hall_id, count(*) FROM dbProj_Reservation"
-                . " WHERE reservation_status_id = 1 or reservation_status_id != 2 "
-                . "GROUP BY hall_id ORDER BY COUNT(*) DESC LIMIT 3";
+        $q = "SELECT r.hall_id, COUNT(*) AS reservation_count 
+      FROM dbProj_Reservation r
+      JOIN dbProj_Hall h ON r.hall_id = h.hall_id
+      WHERE r.reservation_status_id != 2
+      AND h.hall_status_id = 1
+      GROUP BY r.hall_id
+      ORDER BY reservation_count DESC 
+      LIMIT 3";
         $data = $db->multiFetch($q);
         return $data;
     }
-
 }
--- template/home2.php
+++ template/home2.php
@@ -1,59 +1,106 @@
 <?php
+include './helpers/Database.php';
+include './models/Hall.php';
+include './debugging.php';
+?>
+<style>
+    .hero {
+        background: url(./images/hero.png) no-repeat !important;
+        background-size: cover !important;
+        background-position: center !important;
+        height: 40vh !important; 
+    }
+</style>
+<body>
 
-    include './helpers/Database.php';
-    include './models/Hall.php';
-    include './debugging.php';
-    $hall = new Hall();
-    $halls = $hall->getPopularHalls();
-    echo 'number of halls found ' . count($halls);
+    <div class="hero bg-light text-white p-5 mb-5">
+        <div class="container-fluid text-start">
+            <h1 class="display-5 fw-bold">Hall Booking Simplified</h1>
+            <p>Discover and Reserve Your Ideal Space </p>
+            <button class="btn btn-primary btn-lg" onclick="location.href = '#popularHalls'">View Halls</button>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row text-center mb-3">
+            <h1 class="fw-bold">Popular Halls</h1>
+        </div>
+        <div id="popularHalls" class="row d-flex justify-content-center mt-3" >
+            <?php
+            $hall = new Hall();
+            $halls = $hall->getPopularHalls();
+            displayHalls($halls);
+            ?>
+        </div>
+    </div>
+</body>
+
+<?php
+function displayHalls($dataSet) {
+    foreach ($dataSet as $data) {
+        $data = (object) $data;
+        $hall = new Hall();
+        $id = $data->hall_id;
+        $hall->initWithHallid($id);
+        $image = new HallImage();
+        $hallImages = $image->getAllImagesForHall($id);
+        echo '
+        <div class="col-xl-4">
+            <div class="card mb-5 ">
+                <div class="card-body p-0">
+                    <div class="row">
+                        <div class="col">
+                            <div id="carousel-' . $id . '" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-indicators">';
+                        for ($j = 0; $j < count($hallImages); $j++) {
+                            if ($j == 0) {
+                                echo '<button type="button" data-bs-target="#carousel-' . $id . '" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>';
+                            } else {
+                                echo '<button type="button" data-bs-target="#carousel-' . $id . '" data-bs-slide-to="' . ($j) . '" aria-label="Slide ' . ($j) . '"></button>';
+                            }
+                        }
+                           echo'</div>
+                                <div class="carousel-inner rounded-top">';
+                        for ($k = 0; $k < count($hallImages); $k++) {
+                            if ($k == 0) {
+                                echo '<div class="carousel-item active">
+                                        <img src="' . $hallImages[$k]->hall_image_path . '" class="d-block w-100" alt="...">
+                                    </div>';
+                            } else {
+                                echo '<div class="carousel-item">
+                                        <img src="' . $hallImages[$k]->hall_image_path . '" class="d-block w-100" alt="...">
+                                    </div>';
+                            }
+                        }
+
+                            echo'</div> 
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carousel-' . $id . '" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carousel-' . $id . '" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        ';
+        echo '<div class="p-3">';
+            echo '<div class="row text-center">';
+                echo '<h1 class="col">' . $hall->getHallName() . '</h1>'
+                . '</div>';
+            echo '<div class="row"><p class="col text-center text-truncate">' . $hall->getDescription() . '</p></div>';
+            echo '<div class="row align-items-center">';
+                echo '<div class="col"><h5 class="text-end">' . $hall->getRentalCharge() . ' BHD/Hr</h5></div>'
+                    . '<div class="col-auto text-center"><a role="button" href="client_booking.php?hallId=' . $id . '" class="btn btn-primary btn-lg">Book Now</a></div>'
+                    . '<div class="col"><h5 class="text-start">' . $hall->getCapacity() . ' Seats</h5></div>'
+                . '</div>
+                </div>
+            </div>
+        </div>';
+    }
+}
+
 
 ?>
-        <style>
-            .hero {
-                background: url(./images/hero.png) no-repeat;
-                background-size: cover;
-                background-position: center;
-            }
-        </style>
-        <body>
-            
-            <div class="hero bg-light text-white w-100 h-50">
-                <div class="container-fluid">
-                    <h1 class="display-5 fw-bold">Title title title title title title title</h1>
-                    <p class="col-md-8 lead">Lorem ipsum odor amet, consectetuer adipiscing elit. Netus ad quisque. Metus lacus pretium metus. Metus lacus pretium metus. </p>
-                    <button class="btn btn-primary btn-lg" onclick="location.href='#popularHalls'">View Halls</button>
-                </div>
-            </div>
-            
-            <div id="popularHalls">
-               <div class="row text-center"><h3>Popular Halls</h3></div>
-               <div class="row justify-content-center g-2">
-                    <div class="card" style="width: 20rem !important; height: 20rem !important;">
-                        <img src="./images/hero.png" class="card-img-top" alt="" style="height: 75%;">
-                        <div class="card-body">
-                            <p class="card-text">
-                                Lorem ipsum odor amet, consectetuer adipiscing elit. Netus ad quisque. Metus lacus pretium metus. 
-                            </p>
-                        </div>
-                    </div>
-                   
-                   <div class="card" style="width: 20rem !important; height: 20rem !important;">
-                        <img src="./images/hero.png" class="card-img-top" alt="" style="height: 75%;">
-                        <div class="card-body">
-                            <p class="card-text">
-                                Lorem ipsum odor amet, consectetuer adipiscing elit. Netus ad quisque. Metus lacus pretium metus. 
-                            </p>
-                        </div>
-                    </div>
-                   
-                   <div class="card" style="width: 20rem !important; height: 20rem !important;">
-                        <img src="./images/hero.png" class="card-img-top" alt="" style="height: 75%;">
-                        <div class="card-body">
-                            <p class="card-text">
-                                Lorem ipsum odor amet, consectetuer adipiscing elit. Netus ad quisque. Metus lacus pretium metus. 
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </body>
