84bf2e8cbb5eef47aec9595d162eef16259eda18
Hasan <hzs2003@live.com>
2024-05-20 19:49:35 +0300

    fixed admin view reservations query showing every invoice as a booking

--- displayViewBookings.php
+++ displayViewBookings.php
@@ -27,13 +27,13 @@ $table = 'dbProj_Reservation';
 $reservation = new Reservation();
 $reservations = $reservation->getAllReservations($start, $end);
 
-echo count($reservations) . " rows were found";
+//echo "   ". count($reservations) . " rows were found";
 
 $reservation->createReservationsTable($reservations);
 $pagination = new Pagination();
 $pagination->setTotal_records(Reservation::countAllReservations());
 //$pagination->totalRecords($table);
-echo $pagination->total_records . ' is total records';
+//echo $pagination->total_records . ' is total records';
 $pagination->setLimit($end);
 $pagination->page("");
  
