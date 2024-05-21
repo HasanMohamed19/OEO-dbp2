83827b9f530f35d5427a7b5cbf0a9af4263fc04c
Hasan <hzs2003@live.com>
2024-05-21 20:43:43 +0300

    readded jquery to header.php

--- header.php
+++ header.php
@@ -38,9 +38,12 @@ $userName = $_COOKIE['username'];
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <link rel="stylesheet" href="./styles/styles.css">
-
+        
         <!-- fontawesome links-->
-        <script src="https://kit.fontawesome.com/a96d123e51.js" crossorigin="anonymous"></script> 
+        <script src="https://kit.fontawesome.com/a96d123e51.js" crossorigin="anonymous"></script>
+        
+        <!--AJAX-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         
     </head>
     <body>
