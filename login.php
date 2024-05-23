61509886dfef6e7219180d9d20505fa99bbe4956
theMycron <134844972+theMycron@users.noreply.github.com>
2024-05-23 15:35:46 +0300

    upgraded login error message removed Remember me checkbox added login/register image removed login.html (now page is in login.php)

--- login.php
+++ login.php
@@ -1,15 +1,14 @@
 <?php
-include 'header.php';
  include 'debugging.php';
  include './helpers/Database.php';
 include './models/User.php';
 include './models/Login.php';
 
-    if (!empty($_COOKIE['userId'])) {
-        header('Location: displayMyAccount.php');
-    }
+if (!empty($_COOKIE['userId'])) {
+    header('Location: displayMyAccount.php');
+}
 
-    if (isset($_POST['submitted'])) {
+if (isset($_POST['submitted'])) {
     $db = new Database();
     $login = new Login();
     $username = $_POST['username'];
@@ -22,28 +21,61 @@ include './models/Login.php';
          } else {
              header("Location: displayMyAccount.php");
          }
-         
-         echo 'Logged in successfully';
      } else {
-//         header("Location: viewBookings.php");
-         echo '<p class="error"> Wrong Login Values </p>';
+         $errorMessage = 'Incorrect Login Credentials.';
      }
 }
+include 'header.php';
 
 
+?>
+<div class="main">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image rounded-start"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h1 text-gray-900 mb-4">Login</h1>
+                                    </div>
+                                    <form class="user account-form" action="login.php" method="post">
+                                        <input type="text" maxlength="20" class="form-control form-control-user" id="username" placeholder="Username" name="username" value="<?php echo $_POST['username'] ?>">
+                                        <input type="password" maxlength="30" class="form-control form-control-user mb-2" id="password" placeholder="Password" name="password" value="<?php echo $_POST['password'] ?>">
+                                        <div class="row">
+                                                <div class="col-md-6 text-right">
+                                                    <a class="btn btn-link" href="verifyCode.php">Forgot Password?</a>
+                                                </div>
+                                            </div>
+                                        <div class="text-center my-2">
+                                            <!-- <button type="submit" class="btn btn-primary" id="loginBtn">Login</button> -->
+                                            <button type="submit" class="btn btn-primary" id="loginBtn">Login</button>
+                                            <input type="hidden" name="submitted" value="1" />
+                                        </div>
+                                    </form>
+                                    <div class="text-center">
+                                        <p>Don't have an account? <a class="btn-link" href="./register.php">Sign Up</a></p>
+                                    </div>
+                                    <?php
+                                    if ($errorMessage) {
+                                echo '<div id="errorBox" class="text-center text-danger">
+                                        '.$errorMessage.'
+                                    </div>';
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-//include './template/header.html';
-// start_session();
-
-
-//    echo "bababooey lol";
-//    session_start();
-//    setcookie('userId', '3', time() + 60 * 60 * 24 * 7, '/');
-//    echo $_COOKIE['userId'] . " this is the cookie";
-//    echo $_SESSION['userId'] . ": session id";
-echo '<div class="main">';
-include './template/login.html';
-echo  '</div>';
-
+<?php
 include './template/footer.html';
 
